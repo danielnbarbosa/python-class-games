19bcd014bd77e7a74453d866afce28c79f8f403e
Daniel Barbosa <danielnbarbosa@Daniels-MacBook-Pro.local>
2014-10-21 18:03:35 -0700

    first two interesting games

---
+++ memory.py
@@ -0,0 +1,127 @@
+# implementation of card game - Memory
+
+"""
+Explanation of game states:
+0: start of the game, all cards face down
+1: a single exposed, unpaired card
+2: end of a turn, two cards exposed
+"""
+
+import simplegui
+import random
+
+# define constants
+X_CARDS = 4
+Y_CARDS = 4
+CARDS = X_CARDS * Y_CARDS
+CARD_WIDTH = 150
+CARD_HEIGHT = 175
+X_PAD = 20
+Y_PAD = 20
+SLOT_WIDTH = CARD_WIDTH + X_PAD
+SLOT_HEIGHT = CARD_HEIGHT + Y_PAD
+
+# define media assets
+FRONT_IMAGE = simplegui.load_image('http://i.istockimg.com/file_thumbview_approve/6910013/2/stock-illustration-6910013-slot-symbols.jpg')
+FRONT_IMAGE_BW = simplegui.load_image('https://dl.dropboxusercontent.com/u/1237849/stock-illustration-6910013-slot-symbols-black-and-white.jpg')
+FRONT_IMAGE_POS = { 0: (67, 70), 1: (193, 69), 2: (310, 69),
+                   3: (65, 191), 4: (311, 313), 5: (311, 191),
+                   6: (66, 313), 7: (190, 313) }
+BACK_IMAGE = simplegui.load_image('http://media-hearth.cursecdn.com/attachments/2/101/cardback-rankedladder.png')
+CARD_FLIP_SOUND = simplegui.load_sound('http://www.soundrangers.com/demos/gambling/card_deal02.mp3')
+
+# helper function to initialize globals
+def new_game():
+    global cards, exposed, state, card_clicked, card1, card2, match, turns, matched
+    cards = list(range(CARDS / 2)) + list(range(CARDS / 2))
+    random.shuffle(cards)
+    exposed = [False for i in range(CARDS)]
+    matched = [False for i in range(CARDS)]
+    state = 0
+    turns = 0
+    card_clicked = None
+    card1 = None
+    card2 = None
+    match = False
+    
+# define event handlers
+def mouseclick(pos):
+    # add game state logic here
+    global state, card_clicked, card1, card2, match, turns, exposed, matched
+    card_clicked = (pos[0] // SLOT_WIDTH) + (pos[1] // SLOT_HEIGHT * Y_CARDS)
+    # if everything is matched, turn everything color
+    if matched.count(False) == 2:
+        exposed = [True for i in range(CARDS)]
+        matched = [False for i in range(CARDS)] 
+    # evaluate the click only if the card is face down
+    if exposed[card_clicked] == False:
+        exposed[card_clicked] = True
+        # transition from state 0 to state 1
+        if state == 0:
+            state = 1
+            #CARD_FLIP_SOUND.play()
+            card1 = card_clicked
+        # transition from state 1 to state 2
+        elif state == 1:
+            state = 2
+            #CARD_FLIP_SOUND.play()
+            card2 = card_clicked
+            turns += 1
+            if cards[card1] == cards[card2]:
+                match = True
+            else:
+                match = False
+        # transition from state 2 to state 1
+        else:
+            state = 1
+            # flip over unmatched cards
+            if match == True:
+                matched[card1] = True
+                matched[card2] = True
+            else:
+                exposed[card1] = False
+                exposed[card2] = False
+            #CARD_FLIP_SOUND.play()
+            card1 = card_clicked
+  
+def draw(canvas):
+    global cards, exposed, card_clicked, state
+    # update turns
+    label.set_text("Turns = " + str(turns))
+    # display cards either face up or face down accordingly
+    for i in range(CARDS):
+        if exposed[i] == True:
+            if matched[i] == True:
+                # show black and white image if matched
+                canvas.draw_image(FRONT_IMAGE_BW,
+                                  FRONT_IMAGE_POS[cards[i]],
+                                  (125, 125),
+                                  ((i * SLOT_WIDTH + SLOT_WIDTH / 2) % (SLOT_WIDTH * X_CARDS), (SLOT_HEIGHT / 2) + ( i // Y_CARDS * SLOT_HEIGHT)),
+                                  (CARD_WIDTH, CARD_HEIGHT))
+            else:
+                # show color image if not matched
+                canvas.draw_image(FRONT_IMAGE,
+                                  FRONT_IMAGE_POS[cards[i]],
+                                  (125, 125),
+                                  ((i * SLOT_WIDTH + SLOT_WIDTH / 2) % (SLOT_WIDTH * X_CARDS), (SLOT_HEIGHT / 2) + ( i // Y_CARDS * SLOT_HEIGHT)),
+                                  (CARD_WIDTH, CARD_HEIGHT))
+        else:
+            # show back of playing card if face down
+            canvas.draw_image(BACK_IMAGE,
+                              (628 // 2, 934 // 2),
+                              (628, 934),
+                              ((i * SLOT_WIDTH + SLOT_WIDTH / 2) % (SLOT_WIDTH * X_CARDS), (SLOT_HEIGHT / 2) + ( i // Y_CARDS * SLOT_HEIGHT)),
+                              (CARD_WIDTH, CARD_HEIGHT))
+    
+# create frame and add a button and labels
+frame = simplegui.create_frame("Memory", CARDS * SLOT_WIDTH / X_CARDS, CARDS * SLOT_HEIGHT / Y_CARDS)
+frame.add_button("Reset", new_game)
+label = frame.add_label("Turns = 0")
+
+# register event handlers
+frame.set_mouseclick_handler(mouseclick)
+frame.set_draw_handler(draw)
+
+# get things rolling
+new_game()
+frame.start()
---
+++ pong.py
@@ -0,0 +1,135 @@
+# Implementation of classic arcade game Pong
+
+import simplegui
+import random
+
+# initialize globals - pos and vel encode vertical info for paddles
+WIDTH = 600
+HEIGHT = 400       
+BALL_RADIUS = 20
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH / 2
+HALF_PAD_HEIGHT = PAD_HEIGHT / 2
+LEFT = False
+RIGHT = True
+PADDLE_VEL = 5
+
+# initialize ball_pos and ball_vel for new ball in middle of table
+# if direction is RIGHT, the ball's velocity is upper right, else upper left
+def spawn_ball(direction):
+    global ball_pos, ball_vel # these are vectors stored as lists
+    ball_pos = [ WIDTH / 2, HEIGHT / 2 ]
+    x_vel = float(random.randrange(120, 240)) / 60.0
+    y_vel = float(random.randrange(60, 180)) / 60.0
+    if direction == LEFT:
+        ball_vel = [ -x_vel, -y_vel ]
+    elif direction == RIGHT:
+        ball_vel = [ x_vel, -y_vel ]
+        
+# define event handlers
+def new_game():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
+    global score1, score2  # these are ints
+    paddle1_pos = HEIGHT / 2
+    paddle2_pos = HEIGHT / 2
+    paddle1_vel = 0
+    paddle2_vel = 0
+    score1 = 0
+    score2 = 0
+    spawn_ball(RIGHT)
+    
+def draw(canvas):
+    global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
+ 
+    # draw mid line and gutters
+    canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
+    canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
+    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
+        
+    # update ball
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+    
+    # if ball hits left gutter
+    if ball_pos[0] - BALL_RADIUS < 0:
+        # if it hits the left paddle
+        if paddle1_pos - HALF_PAD_HEIGHT < ball_pos[1] < paddle1_pos + HALF_PAD_HEIGHT:
+            # deflect the ball back a little faster
+            ball_vel[0] = -(1.1 * ball_vel[0])
+        else:
+            # give player 2 a point and start new round
+            score2 += 1
+            spawn_ball(RIGHT)
+ 
+    # if ball hits right gutter
+    elif ball_pos[0] + BALL_RADIUS > WIDTH:
+        # if it hits the right paddle
+        if paddle2_pos - HALF_PAD_HEIGHT < ball_pos[1] < paddle2_pos + HALF_PAD_HEIGHT:
+            # deflect the ball back a little faster
+            ball_vel[0] = - (1.1 * ball_vel[0])
+        else:
+            # givel player 1 a point and start new round
+            score1 += 1
+            spawn_ball(LEFT)
+    
+    # handle top and bottom bounce detection
+    if ball_pos[1] - BALL_RADIUS < 0:
+        ball_vel[1] = -ball_vel[1]
+    elif ball_pos[1] + BALL_RADIUS > HEIGHT:
+        ball_vel[1] = -ball_vel[1]
+    
+    # draw ball
+    canvas.draw_circle(ball_pos, BALL_RADIUS, 1, "White", "White")
+    
+    # update paddle's vertical position, keep paddle on the screen
+    if paddle1_pos - HALF_PAD_HEIGHT > 0 and paddle1_vel < 0 \
+    or paddle1_pos + HALF_PAD_HEIGHT < HEIGHT and paddle1_vel > 0:
+        paddle1_pos += paddle1_vel
+    if paddle2_pos - HALF_PAD_HEIGHT > 0 and paddle2_vel < 0 \
+    or paddle2_pos + HALF_PAD_HEIGHT < HEIGHT and paddle2_vel > 0:
+        paddle2_pos += paddle2_vel
+    
+    # draw paddles
+    canvas.draw_line( [HALF_PAD_WIDTH, (paddle1_pos - HALF_PAD_HEIGHT)], [HALF_PAD_WIDTH, (paddle1_pos + HALF_PAD_HEIGHT) ], PAD_WIDTH, 'White')
+    canvas.draw_line( [WIDTH - HALF_PAD_WIDTH, (paddle2_pos - HALF_PAD_HEIGHT)], [WIDTH - HALF_PAD_WIDTH, (paddle2_pos + HALF_PAD_HEIGHT) ], PAD_WIDTH, 'White')
+    
+    # draw scores
+    canvas.draw_text(str(score1), (WIDTH / 2 - 100, HEIGHT / 5 ), 48, 'White', 'sans-serif')
+    canvas.draw_text(str(score2), (WIDTH / 2 + 100, HEIGHT / 5 ), 48, 'White', 'sans-serif')
+    
+# move the paddle as long as the key remains pressed
+def keydown(key):
+    global paddle1_vel, paddle2_vel
+    if key == simplegui.KEY_MAP["W"]:
+        paddle1_vel = -PADDLE_VEL
+    elif key == simplegui.KEY_MAP["S"]:
+        paddle1_vel = PADDLE_VEL
+
+    if key == simplegui.KEY_MAP["up"]:
+        paddle2_vel = -PADDLE_VEL
+    elif key == simplegui.KEY_MAP["down"]:
+        paddle2_vel = PADDLE_VEL
+
+# stop the paddle if the key is no longer pressed
+def keyup(key):
+    global paddle1_vel, paddle2_vel
+    if key == simplegui.KEY_MAP["W"] or key == simplegui.KEY_MAP["S"]:
+        paddle1_vel = 0        
+    if key == simplegui.KEY_MAP["up"] or key == simplegui.KEY_MAP["down"]:
+        paddle2_vel = 0
+
+def reset_button_handler():
+    new_game()
+
+# create frame
+frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+frame.set_keydown_handler(keydown)
+frame.set_keyup_handler(keyup)
+frame.add_button('Reset', reset_button_handler, 200)
+
+
+# start frame
+new_game()
+frame.start()
